f2db015e1ff0785d3b0533848a1e7101e1fa01f2
vithu123 <vithursen.ravi@gmail.com>
2025-06-05 23:44:52 +0200

    Konfigurationsdateien hinzugefügt und aktualisiert

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 // @ts-check
+<<<<<<< HEAD
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
@@ -6,6 +7,15 @@ const angular = require('angular-eslint');
 module.exports = tseslint.config(
   {
     files: ['**/*.ts'],
+=======
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+>>>>>>> 1564cb5 (Konfigurationsdateien hinzugefügt und aktualisiert)
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -14,6 +24,7 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
+<<<<<<< HEAD
       '@angular-eslint/directive-selector': [
         'error',
         {
@@ -28,13 +39,39 @@ module.exports = tseslint.config(
           type: 'element',
           prefix: 'app',
           style: 'kebab-case',
+=======
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+>>>>>>> 1564cb5 (Konfigurationsdateien hinzugefügt und aktualisiert)
         },
       ],
     },
   },
   {
+<<<<<<< HEAD
     files: ['**/*.html'],
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
     rules: {},
   },
+=======
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+>>>>>>> 1564cb5 (Konfigurationsdateien hinzugefügt und aktualisiert)
 );
