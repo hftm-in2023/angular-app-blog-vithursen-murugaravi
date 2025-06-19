49a9bd5ad52c30d5ffd8178f7aaed3277b264e5a
vithu123 <vithursen.ravi@gmail.com>
2025-06-19 20:34:31 +0200

    http setup

---
+++ src/app/material.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  exports: [MatCardModule] // weitere Module hier ergänzen
+})
+export class MaterialModule {}
